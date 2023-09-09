1364be8c999aacf5500462738ed4f1ffa08530a6
ebof1223 <erwinjbofetiado@gmail.com>
2023-09-08 22:31:15 -0500

    init ts

--- token/token.ts
+++ token/token.ts
@@ -1,6 +1,23 @@
-type TokenType = {
-  type: string;
+export type TokenType = string;
+
+export interface Token {
+  type: TokenType;
   literal: string;
-}; 
+}
 
-
+export enum TokenConstants {
+  ILLEGAL = "ILLEGAL",
+  EOF = "EOF",
+  IDENT = "IDENT",
+  INT = "INT",
+  ASSIGN = "=",
+  PLUS = "+",
+  COMMA = ",",
+  SEMICOLON = ";",
+  LPAREN = "(",
+  RPAREN = ")",
+  LBRACE = "{",
+  RBRACE = "}",
+  FUNCTION = "FUNCTION",
+  LET = "LET"
+}
